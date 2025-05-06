bf5b86ec467ce234d178a4e2bddc8a181f94b249
Hajar Bartakh <bartakh.hajar@gmail.com>
2025-05-06 14:11:35 +0000

    Completed seconf phase of the pre-approach

--- attach_shelf/src/pre_approach.cpp
+++ attach_shelf/src/pre_approach.cpp
@@ -12,6 +12,7 @@
 #include "rclcpp/logging.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -23,7 +24,6 @@ public:
     this->declare_parameter("obstacle", 0.0);
     this->declare_parameter("degrees", 0);
     getting_params();
-    // RCLCPP_INFO(this->get_logger(), "Obstacle parameter is : %f", obstacle);
 
     cmd_vel_publisher =
         this->create_publisher<geometry_msgs::msg::Twist>("robot/cmd_vel", 10);
@@ -33,8 +33,8 @@ public:
     scan_sub = this->create_subscription<sensor_msgs::msg::LaserScan>(
         "scan", 10, std::bind(&PreApproachNode::scan_callback, this, _1));
 
-    // odom_sub = this->create_subscription<nav_msgs::msg::Odometry>(
-    //     "odom", 10, std::bind(&PreApproachNode::odom_callback, this, _1));
+    odom_sub = this->create_subscription<nav_msgs::msg::Odometry>(
+        "odom", 10, std::bind(&PreApproachNode::odom_callback, this, _1));
   }
 
 private:
@@ -45,20 +45,39 @@ private:
   }
 
   void timer_callback() {
-    geometry_msgs::msg::Twist twist_cmd;
+    switch (state) {
+    case State::FIRST_STEP:
+      first_step_pre_approach();
+      break;
 
-    // first advance the robot to the desired location
-    //  right in front of the wall
-    if (std::abs(front_distance - obstacle) > 0.1) {
+    case State::FIRST_STEP_DONE:
+      start_heading = current_heading;
+      state = State::SECOND_STEP;
+      break;
+
+    case State::SECOND_STEP:
+      second_step_pre_approach(start_heading);
+      break;
+
+    case State::SECOND_STEP_DONE:
+      RCLCPP_INFO_ONCE(this->get_logger(), "Pre-approach phase is finished!");
+      break;
+    }
+  }
+
+  // The first  step of the preapproach consists of advancing the robot to the
+  // desired location "obstacle" meters away from the wall
+  void first_step_pre_approach() {
+
+    if (std::abs(front_distance - obstacle) > 0.05) {
       RCLCPP_INFO(this->get_logger(), "The front distance is: %f ",
                   front_distance);
-      RCLCPP_INFO(this->get_logger(), "Full speed ahead!!");
+      RCLCPP_INFO_ONCE(this->get_logger(),
+                       "First step of the pre-approach : Moving forward...");
       twist_cmd.linear.x = 0.2;
       twist_cmd.angular.z = 0.0;
-    }
-
-    else {
-      RCLCPP_INFO(this->get_logger(), "Facing wall, stopping...");
+    } else {
+      state = State::FIRST_STEP_DONE;
       twist_cmd.linear.x = 0.0;
       twist_cmd.angular.z = 0.0;
     }
@@ -66,8 +85,32 @@ private:
     cmd_vel_publisher->publish(twist_cmd);
   }
 
-  void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+  // The second step of the preapproach consists of turning the robot to the
+  // desired heading provided by the user in the arguments
+  void second_step_pre_approach(double starting_angle) {
 
+    double target_angle = starting_angle + ((double)degrees * M_PI / 180);
+    double error_angle = current_heading - target_angle;
+
+    if (std::abs(error_angle) > 0.05) {
+
+      twist_cmd.linear.x = 0.0;
+      twist_cmd.angular.z = (error_angle > 0 ? -0.3 : 0.3);
+      RCLCPP_INFO_ONCE(this->get_logger(),
+                       "Second step of the pre-approach: Turning ...");
+      RCLCPP_INFO(this->get_logger(), "error_angle : %f", error_angle);
+
+    } else {
+      twist_cmd.linear.x = 0.0;
+      twist_cmd.angular.z = 0.0;
+      state = State::SECOND_STEP_DONE;
+    }
+    cmd_vel_publisher->publish(twist_cmd);
+  }
+
+  // This callback computes the distance to the nearest obstacle
+  // in front of the robot using the lidar rays
+  void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
     if (!msg->ranges.empty()) {
 
       auto ranges_middle_iterator =
@@ -84,18 +127,22 @@ private:
       front_distance = std::accumulate(front_ranges_copy.begin(),
                                        front_ranges_copy.end(), 0.0) /
                        front_ranges_copy.size();
-
-      // front_distance = msg->ranges[540];
-
-      //   RCLCPP_INFO(this->get_logger(), "The front distance is: %f ",
-      //             front_distance);
     }
   }
 
-  //   void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) const {
+  // This callback provides the current heading of the robot
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
 
-  //     RCLCPP_INFO(this->get_logger(), "I heard odometry data");
-  //   }
+    // Convert odometry orientation quaternion to euler angles
+    tf2::Quaternion quat_tf;
+    geometry_msgs::msg::Quaternion quat_msg = msg->pose.pose.orientation;
+    tf2::fromMsg(quat_msg, quat_tf);
+    double roll{}, pitch{}, yaw{};
+    tf2::Matrix3x3 m(quat_tf);
+    m.getRPY(roll, pitch, yaw);
+
+    current_heading = yaw; // in rads
+  }
 
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher;
@@ -103,8 +150,20 @@ private:
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub;
 
   double front_distance = 0.0;
+  double current_heading = 0.0;
+  double start_heading = 0.0; // heading before rotation of the robot
   double obstacle;
   int degrees;
+
+  geometry_msgs::msg::Twist twist_cmd;
+
+  enum class State {
+    FIRST_STEP,
+    FIRST_STEP_DONE,
+    SECOND_STEP,
+    SECOND_STEP_DONE
+  };
+  State state;
 };
 
 int main(int argc, char *argv[]) {
